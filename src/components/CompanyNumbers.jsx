6be0b706235a9157418309e58cf9fd802835e28b
AmeySwapra <ameyswapratechnologies@gmail.com>
2025-01-16 10:10:14 +0530

    changes has been made to make the homepage responsive

--- src/components/CompanyNumbers.jsx
+++ src/components/CompanyNumbers.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import { Box, Container, Heading, Image, Text, SimpleGrid, GridItem, useBreakpointValue } from '@chakra-ui/react';
-import AOS from 'aos';  // Import AOS library
-import 'aos/dist/aos.css';  // Import AOS CSS
+import { Box, Container, Heading, Image, Text, SimpleGrid, GridItem } from '@chakra-ui/react';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import divider2 from '../assets/bg/divider2.jpg';
 import net from '../assets/logo/net.png';
@@ -10,161 +10,104 @@ import indus from '../assets/logo/indus.png';
 import global from '../assets/logo/global.png';
 import people from '../assets/logo/people.png';
 import hook from '../assets/logo/hook.png';
-import contactbg from '../assets/bg/counter-bg.jpg'; // Import the contactbg image
+import contactbg from '../assets/bg/counter-bg.jpg';
 
 const CompanyNumbers = () => {
   useEffect(() => {
     AOS.init({
-      duration: 1500,  // Duration for animations
-      easing: 'ease-in-out',  // Easing function for smooth transitions
+      duration: 1500,
+      easing: 'ease-in-out',
     });
   }, []);
-
-  const leftImagePosition = useBreakpointValue({
-    base: '-150px',
-    md: '-250px',
-    lg: '-350px',
-  });
-
-  const rightImagePosition = useBreakpointValue({
-    base: '-100px',
-    md: '-150px',
-    lg: '-200px',
-  });
 
   return (
     <Box
       bgImage={`url(${contactbg})`}
       bgSize="cover"
       bgPosition="center"
-      py={{ base: 4, md: 8, lg: 16 }}
-      height="600px"  // Set overall height to 400px
+      py={{ base: 6, md: 8, lg: 16 }}
+      height="auto"
     >
-      <Container minW="1000px">  {/* Set container width to 1000px */}
-        <Heading 
-          as="h2" 
-          textAlign="center" 
-          color="white" 
-          mb={{ base: 4, lg: 8 }} 
-          data-aos="fade-down"
-          data-aos-offset="300"  // Trigger animation 300px before element is in view
-        >
+      <Container maxW="container.xl">
+        <Heading as="h2" textAlign="center" color="white" mb={8} data-aos="fade-down">
           Company in Numbers
         </Heading>
-        
-        <Box textAlign="center" mb={{ base: 4, lg: 8 }} position="relative">
+
+        <Box
+          textAlign="center"
+          mb={8}
+          position="relative"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+          height={{ base: '60px', md: 'auto' }}
+        >
           <Image
             src={divider2}
             alt="heading-hr2"
-            className="heading-hr2"
             data-aos="fade-down"
-            position="absolute"
-            top="50%"
-            left="50%"
-            transform="translate(-50%, -50%)"
+            maxW={{ base: '80%', md: '100%' }}
           />
         </Box>
 
-        <SimpleGrid columns={{ base: 1, lg: 2 }} spacing={{ base: 6, lg: 12 }} mb={{ base: 4, lg: 8 }}>
-          {/* Left Column */}
-          <GridItem 
-            data-aos="fade-left" 
-            data-aos-delay="300"  // Start the animation after 300ms
-            data-aos-offset="200"  // Trigger animation 200px before the element is visible
-            data-aos-duration="2000" // Duration of the animation (2000ms)
-          >
-            <Box p={{ base: 4, lg: 12 }}   borderColor="gray.200">
-              <Box mb={{ base: 4, lg: 8 }}>
-                <Heading as="h3" size="lg" color="white">Global Presence</Heading>
-                <Box display="flex" mt={4} alignItems="center">
-                  <Image
-                    src={set}
-                    alt="Active systems worldwide"
-                    boxSize="50px"
-                    objectFit="contain"
-                  />
-                  <Box ml={4}>
-                    <Text fontSize="2xl" fontWeight="bold" color="white">4000+</Text>
-                    <Text color="white">Active systems worldwide</Text>
-                  </Box>
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          spacing={8}
+          textAlign={{ base: 'center', lg: 'left' }}
+        >
+          <GridItem data-aos="fade-left">
+            <Box>
+              <Heading as="h3" size="md" color="white" mb={6}>
+                Global Presence
+              </Heading>
+              <Box display="flex" flexDirection={{ base: 'column', lg: 'row' }} alignItems="center" mb={6}>
+                <Image src={set} alt="Active systems worldwide" boxSize="50px" />
+                <Box ml={{ lg: 4 }} mt={{ base: 4, lg: 0 }}>
+                  <Text fontSize="xl" fontWeight="bold" color="white">4000+</Text>
+                  <Text color="white">Active systems worldwide</Text>
                 </Box>
               </Box>
-              <Box>
-                <Box display="flex" mt={{ base: 4, lg: 8 }} alignItems="center">
-                  <Image
-                    src={net}
-                    alt="International clients"
-                    boxSize="50px"
-                    objectFit="contain"
-                  />
-                  <Box ml={4}>
-                    <Text fontSize="2xl" fontWeight="bold" color="white">125+</Text>
-                    <Text color="white">International clients</Text>
-                  </Box>
+              <Box display="flex" flexDirection={{ base: 'column', lg: 'row' }} alignItems="center" mb={6}>
+                <Image src={net} alt="International clients" boxSize="50px" />
+                <Box ml={{ lg: 4 }} mt={{ base: 4, lg: 0 }}>
+                  <Text fontSize="xl" fontWeight="bold" color="white">125+</Text>
+                  <Text color="white">International clients</Text>
                 </Box>
-                <Box display="flex" mt={{ base: 4, lg: 8 }} alignItems="center">
-                  <Image
-                    src={global}
-                    alt="Countries of installation"
-                    boxSize="50px"
-                    objectFit="contain"
-                  />
-                  <Box ml={4}>
-                    <Text fontSize="2xl" fontWeight="bold" color="white">50+</Text>
-                    <Text color="white">Countries of installation</Text>
-                  </Box>
+              </Box>
+              <Box display="flex" flexDirection={{ base: 'column', lg: 'row' }} alignItems="center">
+                <Image src={global} alt="Countries of installation" boxSize="50px" />
+                <Box ml={{ lg: 4 }} mt={{ base: 4, lg: 0 }}>
+                  <Text fontSize="xl" fontWeight="bold" color="white">50+</Text>
+                  <Text color="white">Countries of installation</Text>
                 </Box>
               </Box>
             </Box>
           </GridItem>
 
-          {/* Right Column */}
-          <GridItem 
-            data-aos="fade-right" 
-            data-aos-delay="300" 
-            data-aos-offset="200" 
-            data-aos-duration="2000"
-          >
-            <Box p={{ base: 4, lg: 12 }} ml={'40px'}>
-              <Box mb={{ base: 4, lg: 8 }}>
-                <Heading as="h3" size="lg" color="white">Scale and Size</Heading>
-                <Box display="flex" mt={4} alignItems="center">
-                  <Image
-                    src={hook}
-                    alt="Lifting capacity"
-                    boxSize="50px"
-                    objectFit="contain"
-                  />
-                  <Box ml={4}>
-                    <Text fontSize="2xl" fontWeight="bold" color="white">100Ton</Text>
-                    <Text color="white">Lifting capacity</Text>
-                  </Box>
+          <GridItem data-aos="fade-right">
+            <Box>
+              <Heading as="h3" size="md" color="white" mb={6}>
+                Scale and Size
+              </Heading>
+              <Box display="flex" flexDirection={{ base: 'column', lg: 'row' }} alignItems="center" mb={6}>
+                <Image src={hook} alt="Lifting capacity" boxSize="50px" />
+                <Box ml={{ lg: 4 }} mt={{ base: 4, lg: 0 }}>
+                  <Text fontSize="xl" fontWeight="bold" color="white">100Ton</Text>
+                  <Text color="white">Lifting capacity</Text>
                 </Box>
               </Box>
-              <Box>
-                <Box display="flex" mt={{ base: 4, lg: 8 }} alignItems="center">
-                  <Image
-                    src={indus}
-                    alt="M2 manufacturing space"
-                    boxSize="50px"
-                    objectFit="contain"
-                  />
-                  <Box ml={4}>
-                    <Text fontSize="2xl" fontWeight="bold" color="white">30,000m²</Text>
-                    <Text color="white">M² manufacturing space</Text>
-                  </Box>
+              <Box display="flex" flexDirection={{ base: 'column', lg: 'row' }} alignItems="center" mb={6}>
+                <Image src={indus} alt="M2 manufacturing space" boxSize="50px" />
+                <Box ml={{ lg: 4 }} mt={{ base: 4, lg: 0 }}>
+                  <Text fontSize="xl" fontWeight="bold" color="white">30,000m²</Text>
+                  <Text color="white">M² manufacturing space</Text>
                 </Box>
-                <Box display="flex" mt={{ base: 4, lg: 8 }} alignItems="center">
-                  <Image
-                    src={people}
-                    alt="Employees"
-                    boxSize="50px"
-                    objectFit="contain"
-                  />
-                  <Box ml={4}>
-                    <Text fontSize="2xl" fontWeight="bold" color="white">500+</Text>
-                    <Text color="white">Employees</Text>
-                  </Box>
+              </Box>
+              <Box display="flex" flexDirection={{ base: 'column', lg: 'row' }} alignItems="center">
+                <Image src={people} alt="Employees" boxSize="50px" />
+                <Box ml={{ lg: 4 }} mt={{ base: 4, lg: 0 }}>
+                  <Text fontSize="xl" fontWeight="bold" color="white">500+</Text>
+                  <Text color="white">Employees</Text>
                 </Box>
               </Box>
             </Box>
--- src/components/QuoteAndVideoSection.jsx
+++ src/components/QuoteAndVideoSection.jsx
@@ -1,82 +1,89 @@
 import React from "react";
 import { Box, Container, Image, Text, useBreakpointValue } from "@chakra-ui/react";
 import { motion } from "framer-motion";
-import li from '../assets/bg/li.png'
-import ri from '../assets/bg/ri.png'
+import li from "../assets/bg/li.png";
+import ri from "../assets/bg/ri.png";
+
 const MotionBox = motion(Box);
 
 const QuoteAndVideoSection = () => {
-  const isDesktop = useBreakpointValue({ base: false, lg: true });
-
-  const quoteAnimation = {
-    hidden: { opacity: 0 },
-    visible: {
-      opacity: 1,
-      transition: { duration: 8 },
-    },
-  };
-
   const leftImagePosition = useBreakpointValue({
-    base: "-150px",
-    md: "-250px",
-    lg: "-350px",
+    base: "-40px",
+    sm: "-60px",
+    md: "-100px",
+    lg: "-150px",
   });
 
   const rightImagePosition = useBreakpointValue({
-    base: "-100px",
-    md: "-150px",
-    lg: "-200px",
+    base: "-40px",
+    sm: "-60px",
+    md: "-100px",
+    lg: "-150px",
   });
 
   return (
-    <Box py={{ base: 4, md: 8, lg: 24 }}>
-      <Container>
+    <Box
+      py={{ base: 6, md: 10, lg: 24 }}
+      overflowX="hidden" // Prevent horizontal scrolling
+    >
+      <Container maxW="container.lg" px={{ base: 4, md: 8 }}>
+        {/* Quote Section */}
         <Box
           display="flex"
           alignItems="center"
           justifyContent="center"
           position="relative"
           textAlign="center"
-          mb={{ base: 6, lg: 12 }}
+          mb={{ base: 8, md: 12 }}
+          overflow="hidden" // Ensure images do not overflow
         >
+          {/* Left Quote Icon */}
           <Image
             src={li}
             alt="Left Quote Icon"
             position="absolute"
             left={leftImagePosition}
-            top="0"
+            top={{ base: "50%", lg: "0" }}
             transform="translateY(-50%)"
             zIndex={1}
+            w={{ base: "30px", sm: "40px", md: "60px", lg: "80px" }}
           />
+          {/* Quote Text */}
           <MotionBox
             className="quote-text"
-            minWidth="1000px"
-            maxWidth="100%"
+            maxWidth={{ base: "100%", md: "90%", lg: "80%" }}
             mx="auto"
             px={{ base: 4, md: 8 }}
             pt={4}
             pb={6}
-            initial="hidden"
-            animate="visible"
-            variants={quoteAnimation}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 8 }}
           >
-            <Text fontSize={{ base: "18px", md: "22px", lg: "30px" }} fontWeight="bold">
+            <Text
+              fontSize={{ base: "16px", sm: "18px", md: "22px", lg: "30px" }}
+              fontWeight="bold"
+              lineHeight="1.5"
+            >
               Our Mission: To be a world leader in energy &amp; process equipment with a focus on
               <br />
               realising the transition to clean energy.
             </Text>
           </MotionBox>
+          {/* Right Quote Icon */}
           <Image
             src={ri}
             alt="Right Quote Icon"
             position="absolute"
             right={rightImagePosition}
-            top="100%"
+            top={{ base: "50%", lg: "100%" }}
             transform="translateY(-50%)"
             zIndex={1}
+            w={{ base: "30px", sm: "40px", md: "60px", lg: "80px" }}
           />
         </Box>
 
+        {/* Video Section */}
         <Box
           display="flex"
           alignItems="center"
@@ -84,15 +91,17 @@ const QuoteAndVideoSection = () => {
           py={{ base: 4, md: 8 }}
           borderRadius="md"
         >
-          <iframe
+          <Box
+            as="iframe"
             width="100%"
-            height="315"
+            maxWidth="800px"
+            height={{ base: "200px", sm: "250px", md: "315px" }}
             src="https://www.youtube.com/embed/DPDFUcAMySQ"
             title="YouTube video"
             frameBorder="0"
             allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
             allowFullScreen
-          ></iframe>
+          ></Box>
         </Box>
       </Container>
     </Box>
